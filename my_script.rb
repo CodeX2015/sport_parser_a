8bcc280662a558720707429a630ec1772858de7a
CodeX2015 <prodazha@list.ru>
2018-04-24 21:54:08 +0300

    get src data  from forum

--- my_script.rb
+++ my_script.rb
@@ -11,19 +11,36 @@ require 'nokogiri'
   table = page.css("table[style='border-collapse: collapse;']")
 
 #puts table
+i = 0
 
   table.css('tr').each do |tr|
+    puts "tr=#{i}"
+    j=0
+    k=0
+    i+=1
     tr.css('td').each do |td|
-      #puts td
+      puts "td=#{j}"
+
+      j+=1
       puts td.text
                #.css('.myTblTD1')
       #puts 'next'
+    end
+    tr
+    if tr.css('a[href]') != nil then
+      tr.css('a').each do |link|
+        puts "link=#{k}"
+        k+=1
+        #puts link
+        #puts link["title"][0].text
+        puts link['href']
+      end
     end
     # data = Hash.new
     #
     # data['date'] = link.css(".myTblTD1")
     #
     # data['href'] = link['href']
-  #break
+  if i==11 then break end
 
 end
